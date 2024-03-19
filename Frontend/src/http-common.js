7cd62e937fe923b5b7648c63a2dd17ab462c3576
Raghavendiran-2002 <raghavendiran46461@gmail.com>
2024-03-19 18:52:50 +0530

    docker v0.9

--- Frontend/src/http-common.js
+++ Frontend/src/http-common.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL:
-    process.env.REACT_APP_API_BASE_URL || "http://192.168.1.200:8080/api",
+  baseURL: "http://192.168.1.200:6868/api",
   headers: {
     "Content-type": "application/json",
   },
