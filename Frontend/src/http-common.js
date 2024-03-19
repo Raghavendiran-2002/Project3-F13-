b25cbc8c54963a30c19b954087a5b9ec1a58411a
Raghavendiran-2002 <raghavendiran46461@gmail.com>
2024-03-19 19:08:27 +0530

    urlupdated

--- Frontend/src/http-common.js
+++ Frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://192.168.1.200:6868/api",
+  baseURL: "http://api.raghavendiran.tech/api",
   headers: {
     "Content-type": "application/json",
   },
