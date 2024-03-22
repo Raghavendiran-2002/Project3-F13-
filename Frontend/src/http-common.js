fb3f2a601863e7b974ef127abf74a8831611aacc
Raghavendiran-2002 <raghavendiran46461@gmail.com>
2024-03-22 15:53:12 +0530

    aws config

--- Frontend/src/http-common.js
+++ Frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://api.raghavendiran.tech/api",
+  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8080/api",
   headers: {
     "Content-type": "application/json",
   },
